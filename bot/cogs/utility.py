9f3b9717583d28fa39407db655d47f3a313e82af
@TheIceLeopard <10292779+StormyIceLeopard@users.noreply.github.com>
2021-03-15 20:20:58 -0400

    Implemented lockdown command
    Currently configured, lockdown.txt must already exist with '0' inside.

--- bot/cogs/utility.py
+++ bot/cogs/utility.py
@@ -21,7 +21,26 @@ class Utility(commands.Cog):
         """Reloads filters"""
         FileProcessor.load()
         await ctx.send("Reloaded!")
-
+        
+    @commands.command(name="lockdown")
+    @bot_global.bot_manager()
+    async def reload_filters(self, ctx):
+        """Locks down the server, no more !agree"""
+        with open('./lockdown.txt', 'r+') as file:
+            currentState = file.read()
+            if currentState == '1': # Server is in lockdown
+                file.seek(0) # Move cursor to start of file
+                file.write('0') # Change lockdown to false
+                file.truncate() # Finish writing to file
+                await ctx.channel.set_permissions(ctx.guild.default_role, send_messages=True) # Disable lockdown
+            elif currentState == '0': # Server is not in lockdown
+                file.seek(0) # Move cursor to start of file
+                file.write('1') # Change lockdown to true
+                file.truncate() # Finish writing to file
+                await ctx.channel.set_permissions(ctx.guild.default_role, send_messages=False) # Enable lockdown
+            else: # File is not an expected value, send error to console
+                print("Unexpected value in 'lockdown.txt' file. Received: " + currentState)
+                
     @commands.command(name="agree")
     async def agree(self, ctx):
         if ctx.channel.id != 720394587792867378:
