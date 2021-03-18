c3564f8a80b0ffa6a48aa1607ad7216f46954981
Benjamin Grusendorf <benjamin@GRUSENDORF.LAN>
2021-03-18 17:51:58 -0600

    Added a way of checking if the person sending the messages is the manager Added vscode exceptions to the gitignore.

--- bot/cogs/message_filter.py
+++ bot/cogs/message_filter.py
@@ -15,7 +15,7 @@ class MessageFilter(commands.Cog):
     async def on_message(self, message):
         if message.guild is None or message.author == self.bot.user:
             return
-        if message.channel.id == 720394587792867378 or message.author == self.bot.user:
+        if message.channel.id == 720394587792867378 or message.author == self.bot.user and not await bot_global.is_bot_manager(message.author):
             if message.content != "!agree":
                 await message.delete()
                 return
